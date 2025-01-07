040540f3e35aa061808ab192ef237ca88fc4ac68
budhil <budhilnigam@gmail.com>
2025-01-07 23:51:04 +0530

    user model

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,9 +3,8 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB connected');
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log('Error:',error.message);
-        process.exit(1);
+        console.log('MongoDB connection error:',error.message);
     }
 };
