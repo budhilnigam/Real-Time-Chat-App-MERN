bb213c4b1bb9fbea41e8380c3b24430b6377b925
budhil <budhilnigam@gmail.com>
2025-01-11 23:59:41 +0530

    cookie parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +12,8 @@ const PORT= process.env.PORT;
 
 //middleware to extract daat from request body
 app.use(express.json());
+//middleware to parse cookies
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
