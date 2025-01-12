6dae76bc592ac89400dc387a19a20d13fbc54d69
budhil <budhilnigam@gmail.com>
2025-01-12 23:04:00 +0530

    backend for update profile and auth check added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -88,3 +88,35 @@ export const logout = (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId =req.user._id;
+
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile Picture is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in update profile:",error);
+        res.status(500).json({ message: "Internal server errror"});
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200)
+    } catch (error) {
+        console.log("Error in checkAuth controller:",error.message);
+        res.status(500).json({ message: "Internal Server Error"});
+    }
+}
