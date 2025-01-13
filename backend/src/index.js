5551b719d50114500b3f521d3c1ee2a005eb415b
budhil <budhilnigam@gmail.com>
2025-01-13 23:47:52 +0530

    using app declared in socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js"
 
 dotenv.config();
-const app=express();
 
 const PORT= process.env.PORT;
 
@@ -15,7 +16,12 @@ const PORT= process.env.PORT;
 app.use(express.json());
 //middleware to parse cookies
 app.use(cookieParser());
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
