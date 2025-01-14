4941a86e160d0461f51ac630862173261a57319e
budhil <budhilnigam@gmail.com>
2025-01-14 16:59:38 +0530

    Frontend complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
 
   return (
     <header
@@ -11,7 +14,10 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
-              <h1 className="text-lg font-bold">SupaChat</h1>
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">Chatty</h1>
             </Link>
           </div>
 
@@ -23,16 +29,19 @@ const Navbar = () => {
               
               `}
             >
+              <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
             </Link>
 
             {authUser && (
               <>
                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button className="flex gap-2 items-center">
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
               </>
